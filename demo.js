bcf48f4f006c646851c8aa96c8b4d475f8098c06
GuessFinger <767329607@qq.com>
2024-11-23 20:02:40 +0800

    格式化

---
+++ demo.js
@@ -0,0 +1,12 @@
+const obj = {
+    name: "ez",
+    age: 21,
+    address: {
+        city: "beijing",
+        country: "china",
+    },
+};
+
+const changeName = (name) => {
+    return name.split("").reverse().join("-");
+};
